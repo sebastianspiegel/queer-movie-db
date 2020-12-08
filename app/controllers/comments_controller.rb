6e406bbe6291fdc5af581914ebddb8a24bb781bc
sebastianspiegel <sebastian.j.spiegel@gmail.com>
2020-12-08 13:38:47 -0500

    validation of comments and correction of belongs to in model

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,9 @@
 class CommentsController < ApplicationController
 
     post '/comments' do
+        #make sure correct user and logged in
         binding.pry 
+        Comment.create(params)
+        redirect "/movies/#{params[:movie_id]}"
     end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,5 @@
 class Comment < ActiveRecord::Base
     belongs_to :movie
+    belongs_to :user
+    validates :content, presence: true
 end
